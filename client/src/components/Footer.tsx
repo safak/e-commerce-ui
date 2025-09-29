c909f504879922d1350b10e7b37303bad9bca019
Ali Khani <khaniali568@gmail.com>
2025-09-29 11:22:06 +0330

    footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-16 flex flex-col items-center md:flex-row md:items-start bg-gray-800 p-8 rounded-lg">
+      <div className="">
+        <Link href={"/"} className="flex items-center">
+          <Image src={"/logo.png"} alt="Trend Lama" width={36} height={36} />
+          <p className="hidden md:block font-medium text-md tracking-wider">
+            TRENDLAMA.
+          </p>
+        </Link>
+        <p>h 2025 Alikhanix</p>
+        <p>All rights reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
