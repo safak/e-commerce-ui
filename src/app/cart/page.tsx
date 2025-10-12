74a99231629adec361191188968f878b8f87d132
Camelia <cameliaafolabi@gmail.com>
2025-10-12 04:13:10 +0100

    created shipping form, payment form components and validation

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,12 @@
 "use client"
+
+import { PaymentForm } from "@/components/PaymentForm";
+import ShippingForm from "@/components/ShippingForm";
 import { CartsType } from "@/types/types";
 import { ArrowRight, Trash2 } from "lucide-react";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const CartPage = () => {
   const steps = [
@@ -69,10 +72,11 @@ const CartPage = () => {
 
   const param = useSearchParams();
   const router = useRouter();
+  const [shippingForm, setShippingForm] = useState(null)
 
   const activeStep = parseInt(param.get("step") || "1")
   return (
-    <div className="flex flex-col gap-8 items-center justify-center mt-12">
+    <div className="flex flex-col gap-8 items-center justify-center my-12">
       <h1 className="text-2xl font-medium">Shopping Cart</h1>
       <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16">
         {steps.map((step) => (
@@ -147,17 +151,13 @@ const CartPage = () => {
                   </button>
                 </div>
               ))
-            : activeStep === 2 && (
-                <></>
-                // <ShippingForm setShippingForm={setShippingForm} />
-                //   ) : activeStep === 3 && shippingForm ? (
-                //     <PaymentForm />
-                //   ) : (
-                //     <p className="text-sm text-gray-500">
-                //       Please fill in the shipping form to continue.
-                //     </p>
-                //   )
-              )}
+            : activeStep === 2 ? (
+                <ShippingForm setShippingForm={setShippingForm}  />
+            ):(
+              activeStep === 3 && shippingForm ? (<PaymentForm />) : (<p className="text-sm text-gray-500">Please fill in the shipping form to continue.</p>)
+              
+            )
+            }
         </div>
         {/* Details */}
         <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8 h-max">
@@ -177,7 +177,14 @@ const CartPage = () => {
             <hr className="border-gray-200" />
             <div className="flex justify-between">
               <p className="text-gray-800 font-semibold">Total</p>
+              <p className="font-medium">
+                $
+                {cartItem
+                  .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </p>
             </div>
+             {activeStep === 1 && (
             <button
               onClick={() => router.push("/cart?step=2", { scroll: false })}
               className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2"
@@ -185,6 +192,7 @@ const CartPage = () => {
               Continue
               <ArrowRight className="w-3 h-3" />
             </button>
+          )}
           </div>
         </div>
       </div>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export type ProductType = {
   id: string | number;
   name: string;
@@ -18,3 +20,30 @@ export type CartItemType = ProductType & {
 }
 
 export type CartsType = CartItemType[];
+
+export const shippingFormSchema = z.object({
+  name: z.string().min(1, "Name is Required"),
+  email: z.email().min(1, "Email is Required"),
+  phone: z.string().min(1, "Number is Required").max(10, "Maximum number is 10").regex(/^(\+?1[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/, "Input the right format of using number"),
+  address: z.string().min(1, "Address is required"),
+  city: z.string().min(1, "City is required"),
+})
+
+export type ShoppingFormInput = z.infer<typeof shippingFormSchema>
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required"),
+  cardNumber: z
+    .string()
+    .min(16, "Card Number is required")
+    .max(16, "Card Number is required"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/\d{2}$/,
+      "Expiration date must be in MM/YY format"
+    ),
+  cvv: z.string().min(3, "CVV is required").max(3, "CVV is required"),
+})
+
+export type PaymentFormInput = z.infer<typeof paymentFormSchema>
