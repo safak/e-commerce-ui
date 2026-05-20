718b207c138060b040ee0b956aad44ac88f71fd5
Imesh Bandara <imeshnbandara826@gmail.com>
2026-05-21 01:01:26 +0530

    added some parts to nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = ()=>{
     return (
-        <div className="">Navbar</div>
+        <nav className="w-full flex item-center justify-between border-b border-gray-200 pb-4">
+            {/*Left*/}
+            <Link href ="/" className="flex items-center">
+            <Image 
+            src="/logo.png"
+            alt = "LuxeFit Clothing"
+            width={36}
+            height={36}
+            className="w-6 h-6 md:w-9 md:h-9"
+            />
+            <p className="text-md font-medium">LUXEFIT CLOTHING</p>
+                </Link>
+
+            {/*Right*/}
+            <div>Right</div>
+        </nav>
+
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Homepage = () => {
   return (
-    <div className='bg-red-400'>Homepage</div>
+    <div className=''>Homepage</div>
   )
 }
 
